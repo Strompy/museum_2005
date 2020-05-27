ee1936e7093a2fa9e1df44cf8569127d08e8dc07
Phillip Strom <phillip.strom@gmail.com>
2020-05-27 09:10:59 -0600

    Fix typo in exists

--- test/exhibit_test.rb
+++ test/exhibit_test.rb
@@ -3,7 +3,7 @@ require 'minitest/autorun'
 require 'minitest/pride'
 
 class ExhibitTest < Minitest::Test
-  def test_it_exits
+  def test_it_exists
     exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
     assert_instance_of Exhibit, exhibit
   end
