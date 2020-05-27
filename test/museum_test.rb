535341fa0caba8ca2dfccb504e3b808504b2a5cc
Phillip Strom <phillip.strom@gmail.com>
2020-05-27 09:49:17 -0600

    Add setup

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -5,52 +5,45 @@ require 'minitest/autorun'
 require 'minitest/pride'
 
 class MuseumTest < Minitest::Test
+
+  def setup
+    @dmns = Museum.new("Denver Museum of Nature and Science")
+    @gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    @dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    @imax = Exhibit.new({name: "IMAX",cost: 15})
+    @dmns.add_exhibit(@gems_and_minerals)
+    @dmns.add_exhibit(@dead_sea_scrolls)
+    @dmns.add_exhibit(@imax)
+    @patron_1 = Patron.new("Bob", 20)
+    @patron_2 = Patron.new("Sally", 20)
+    @patron_1.add_interest("Dead Sea Scrolls")
+    @patron_1.add_interest("Gems and Minerals")
+    @patron_2.add_interest("IMAX")
+    @dmns.add_patron(@patron_1)
+    @dmns.add_patron(@patron_2)
+
+  end
   def test_it_exists
-    dmns = Museum.new("Denver Museum of Nature and Science")
-    assert_instance_of Museum, dmns
+    assert_instance_of Museum, @dmns
   end
 
   def test_it_has_attributes
     dmns = Museum.new("Denver Museum of Nature and Science")
+
     assert_equal "Denver Museum of Nature and Science", dmns.name
     assert_equal [], dmns.exhibits
   end
 
   def test_it_can_add_exhibits
-    dmns = Museum.new("Denver Museum of Nature and Science")
-    gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
-    dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
-    imax = Exhibit.new({name: "IMAX",cost: 15})
-    dmns.add_exhibit(gems_and_minerals)
-    dmns.add_exhibit(dead_sea_scrolls)
-    dmns.add_exhibit(imax)
-    assert_equal [gems_and_minerals, dead_sea_scrolls, imax], dmns.exhibits
+    assert_equal [@gems_and_minerals, @dead_sea_scrolls, @imax], @dmns.exhibits
   end
 
   def test_it_can_have_patrons
-    dmns = Museum.new("Denver Museum of Nature and Science")
-    patron_1 = Patron.new("Bob", 20)
-    patron_2 = Patron.new("Sally", 20)
-    dmns.add_patron(patron_1)
-    dmns.add_patron(patron_2)
-    assert_equal [patron_1, patron_2], dmns.patrons
+    assert_equal [@patron_1, @patron_2], @dmns.patrons
   end
 
   def test_it_can_recommend_exhibits
-    dmns = Museum.new("Denver Museum of Nature and Science")
-    gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
-    dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
-    imax = Exhibit.new({name: "IMAX",cost: 15})
-    dmns.add_exhibit(gems_and_minerals)
-    dmns.add_exhibit(dead_sea_scrolls)
-    dmns.add_exhibit(imax)
-    patron_1 = Patron.new("Bob", 20)
-    patron_2 = Patron.new("Sally", 20)
-    patron_1.add_interest("Dead Sea Scrolls")
-    patron_1.add_interest("Gems and Minerals")
-    patron_2.add_interest("IMAX")
-    
-    assert_equal [dead_sea_scrolls, gems_and_minerals], dmns.recommend_exhibits(patron_1)
-    assert_equal [imax], dmns.recommend_exhibits(patron_2)
+    assert_equal [@dead_sea_scrolls, @gems_and_minerals], @dmns.recommend_exhibits(@patron_1)
+    assert_equal [@imax], @dmns.recommend_exhibits(@patron_2)
   end
 end
