a2dbe88afdc4b6c61eb8a425bfda1d2cf062f505
Phillip Strom <phillip.strom@gmail.com>
2020-05-27 09:23:29 -0600

    Add exhibit attribute

--- lib/museum.rb
+++ lib/museum.rb
@@ -2,8 +2,9 @@ require './lib/patron'
 require './lib/exhibit'
 
 class Museum
-  attr_reader :name
+  attr_reader :name, :exhibits
   def initialize(name)
     @name = name
+    @exhibits = []
   end
 end
